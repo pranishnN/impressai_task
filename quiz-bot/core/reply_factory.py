d8c784b66bf25be307f80fa16173e816aff57a42
pranishnN <pranishnallasivam512@hotmail.com>
2024-02-09 13:23:47 +0530

    my first commit

---
+++ quiz-bot/core/reply_factory.py
@@ -0,0 +1,74 @@
+
+from .constants import BOT_WELCOME_MESSAGE, PYTHON_QUESTION_LIST
+
+
+def generate_bot_responses(message, session):
+    bot_responses = []
+
+    current_question_id = session.get("current_question_id")
+    if not current_question_id:
+        bot_responses.append(BOT_WELCOME_MESSAGE)
+
+    success, error = record_current_answer(message, current_question_id, session)
+
+    if not success:
+        return [error]
+
+    next_question, next_question_id = get_next_question(current_question_id)
+
+    if next_question:
+        bot_responses.append(next_question)
+    else:
+        final_response = generate_final_response(session)
+        bot_responses.append(final_response)
+
+    session["current_question_id"] = next_question_id
+    session.save()
+
+    return bot_responses
+
+
+def record_current_answer(answer, current_question_id, session):
+    '''
+    Validates and stores the answer for the current question to django session.
+    '''
+    if current_question_id is not None and current_question_id != "": 
+        session[f"answer_{current_question_id}"] = answer
+        session.save()
+
+    return True, ""
+
+
+def get_next_question(current_question_id):
+    '''
+    Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
+    '''
+    if current_question_id is None:
+        return PYTHON_QUESTION_LIST[0],0
+
+    next_question_id = current_question_id + 1
+    if next_question_id < len(PYTHON_QUESTION_LIST):
+        return PYTHON_QUESTION_LIST[next_question_id], next_question_id
+    else:
+        return None, next_question_id
+
+    
+def generate_final_response(session):
+    '''
+    Creates a final result message including a score based on the answers
+    by the user for questions in the PYTHON_QUESTION_LIST.
+    '''
+
+    total_questions = len(PYTHON_QUESTION_LIST)
+    correct_answers = 0
+
+    for question_id, question in enumerate(PYTHON_QUESTION_LIST):
+        user_answer = session.get(f"answer_{question_id}")
+        if user_answer and user_answer == question['answer']:
+            correct_answers += 1
+
+    score = (correct_answers / total_questions) * 100
+
+    final_message = f"You scored {score:.2f}% with {correct_answers} out of {total_questions} correct."
+
+    return final_message
